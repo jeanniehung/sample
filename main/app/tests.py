2f0b249132af8459d56124701c3f085d9a336a2d
jeannie <18523883657@163.com>
2020-12-09 15:05:18 +0800

    index.html

--- main/app/tests.py
+++ main/app/tests.py
@@ -1,3 +1,7 @@
+from django.urls import reverse
 from django.test import TestCase
 
 # Create your tests here.
+class IndexTest(TestCase):
+
+    pass
--- main/app/views.py
+++ main/app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('xx')
+    boards = Board.objects.all()
+    # response_html = '<br>'.join('%s' %id for id in boards)
+    # return HttpResponse(response_html)
+    return render(request, 'index.html', {'boards': boards})
+
+
+
