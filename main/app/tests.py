29e19c4419e4f744aaf64e839e9683d55ab1baa1
jeannie <18523883657@163.com>
2020-12-10 11:53:00 +0800

    test

--- main/app/tests.py
+++ main/app/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.test import client
 from django.urls import reverse
 from app import views
+from .models import Board
 
 # Create your tests here.
 
@@ -27,5 +28,21 @@ class IndexTest(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/index/')  #ResolverMatch(func=app.views.index, args=(), kwargs={}, url_name=app-index, app_names=[], namespaces=[], route=^index/$)
         self.assertEqual(view.func, views.index)
+
+
+class BoardTopicsTests(TestCase):
+
+    def setUp(self) -> None:
+        Board.objects.create(name='ZEGO', description='zegodescrptions')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        c = client.Client()
+        response = c.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_board_topics_url_resolves_home_view(self):
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func, views.board_topics)
 
 
--- main/app/views.py
+++ main/app/views.py
@@ -11,6 +11,7 @@ def index(request):
     # return HttpResponse(response_html)
     return render(request, 'index.html', {'boards': boards})
 
+
 def board_topics(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'topics.html', {'board': board})
