3c80cb388bfa7f32afad9447d7686c3905eab03a
jeannie <18523883657@163.com>
2020-12-10 08:27:27 +0800

    topics.html

--- main/main/urls.py
+++ main/main/urls.py
@@ -21,5 +21,5 @@ from app import views as app
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^index/$', app.index, name='app-index'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics')
+    url(r'^boards/(?P<pk>\d+)/$', app.board_topics, name='board_topics')
 ]
