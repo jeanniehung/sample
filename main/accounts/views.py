14b42f490e17d8e74f0597283b353aec9bccde13
jeannie <18523883657@163.com>
2020-12-14 10:13:29 +0800

    add

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
--- main/main/settings.py
+++ main/main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'widget_tweaks',
+    'accounts',
 ]
 
 MIDDLEWARE = [
