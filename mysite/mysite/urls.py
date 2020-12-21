82c2a716c0093d82e23887107acf7625cf6db6d3
Jeannie <18523883657@163.com>
2020-12-21 16:21:56 +0800

    xx

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""myapp URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,13 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.index, name='index'),
-    url(r'^$', views.index, name='index'),
-    # path('boards/<int:pk>/', views.board_topics, name='board_topics'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
 ]
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from boards.models import Board
 
 # Register your models here.
-admin.site.register(Board)
