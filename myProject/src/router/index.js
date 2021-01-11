b9cc263150cb3db0aa22b5403ab30d252623c47e
Jeannie <18523883657@163.com>
2021-01-11 19:12:38 +0800

    ue

--- myProject/src/router/index.js
+++ myProject/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/component/about'
+import Recruit from '@/components/recruit'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      parh: '/recruit',
+      name: 'recruit',
+      component: Recruit
     }
   ]
 })
